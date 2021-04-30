475c2b3378193a1c88c63e3ed32bc6069403acdd
amofogh <amir.ofogh81@gmail.com>
2021-05-01 01:39:57 +0430

    V1.0.0

---
+++ index.js
@@ -0,0 +1,108 @@
+$(document).ready(function () {
+  ClearInput();
+
+  // style for select and option
+  $(".seluser").niceSelect();
+
+  // cmt: eventlistener for use enter in inputs
+  $("#Weight , #Height").on("keydown", function (e) {
+    if (e.key == "Enter") {
+      BMI();
+    }
+  });
+
+  // Int Checker
+  function BMI() {
+    let Height = $("#Height").val();
+    let Weight = $("#Weight").val();
+
+    if (isNaN(Weight && Height)) {
+      alert("Not A Number");
+      GetType(false);
+    } else if (Weight == "" || Height == "") {
+      alert("Fill the Fields !");
+      GetType(false);
+    } else {
+      GetType(true);
+    }
+  }
+
+  // cmt: get option value for kg or pound and cm or inch
+  function GetType(State) {
+    let Weight;
+    let Height;
+
+    let typeWeight = $(".select:eq(0)").val();
+    let typeHeight = $(".select:eq(2)").val();
+
+    let WeighttVal = $("#Weight").val();
+    let HeightVal = $("#Height").val();
+
+    if (typeWeight == "Pound") {
+      // convet pound to kg
+      Weight = WeighttVal / 2.205;
+    } else if (typeWeight == "Kg") {
+      Weight = WeighttVal;
+    }
+
+    if (typeHeight == "Inch") {
+      // convet Inch to cm
+      Height = HeightVal * 2.54;
+    } else if (typeHeight == "Cm") {
+      Height = HeightVal;
+    }
+
+    BMICalculator(State, Weight, Height);
+  }
+
+  // cmt: get the data from Gettype and intChecker then calc BMI
+  function BMICalculator(State, Weight, Height) {
+    if (State == true) {
+      //BMI formula    Height to M then **2
+      var Result = (Weight / (Height / 100) ** 2).toFixed(2);
+
+      // Set WeightState by BMI
+      let WeightStatus = WeightScanner(Result);
+
+      $(".demo").text(Result);
+      $(".show").text(WeightStatus);
+
+      ClearInput();
+    }
+  }
+
+  // set WeightStatus by your BMI number
+  function WeightScanner(Result) {
+    let WeightStatus;
+
+    if (Result < 18.5) {
+      WeightStatus = "UnderWeight";
+    } else if (Result >= 18.5 && Result < 25) {
+      WeightStatus = "Normal";
+    } else if (Result >= 25 && Result < 30) {
+      WeightStatus = "OverWeight";
+    } else if (Result >= 30 && Result < 35) {
+      WeightStatus = "Obesity class I";
+    } else if (Result >= 35 && Result < 40) {
+      WeightStatus = "Obesity class II";
+    } else if (40 < Result) {
+      WeightStatus = "Obesity class III";
+    }
+
+    return WeightStatus;
+  }
+
+  function ClearInput() {
+    $("#Height").val("");
+    $("#Weight").val("");
+  }
+
+  $(".btn").click(BMI);
+
+  // cmt: Picture
+  $(".Chart").click(function () {
+    $(this).toggleClass("openPic");
+    $(".Img").toggleClass("Zoom-img");
+    $(".black").toggleClass("block");
+  });
+});
